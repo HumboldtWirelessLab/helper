320f38db8d232fd0188b53fc47922e3feac0d708
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2008-06-19 13:36:40 +0200

    small changes

--- nodes/bin/wlanmodules.sh
+++ nodes/bin/wlanmodules.sh
@@ -15,19 +15,19 @@ case "$1" in
 	echo insmod ath_rate_sample
 	insmod ${MODULSDIR}/ath_rate_sample.ko
 
-	#insmod ${MODULSDIR}/wlan_acl.ko
-	#insmod ${MODULSDIR}/wlan_ccmp.ko
-	#insmod ${MODULSDIR}/wlan_scan_ap.ko
-	#insmod ${MODULSDIR}/wlan_scan_sta.ko
-	#insmod ${MODULSDIR}/wlan_tkip.ko
-	#insmod ${MODULSDIR}/wlan_wep.ko
-	#insmod ${MODULSDIR}/wlan_xauth.ko
+	insmod ${MODULSDIR}/wlan_acl.ko
+	insmod ${MODULSDIR}/wlan_ccmp.ko
+	insmod ${MODULSDIR}/wlan_scan_ap.ko
+	insmod ${MODULSDIR}/wlan_scan_sta.ko
+	insmod ${MODULSDIR}/wlan_tkip.ko
+	insmod ${MODULSDIR}/wlan_wep.ko
+	insmod ${MODULSDIR}/wlan_xauth.ko
 
 	echo insmod ath_pci $ATH_PCI
 	insmod ${MODULSDIR}/ath_pci.ko $ATH_PCI 
 	;;
     "uninstall")
-	MODLIST="wlan_scan_ap ath_pci ath_rate_sample ath_rate_minstrel wlan ath_hal hostap_pci hostap ieee80211_crypt"
+	MODLIST="wlan_xauth wlan_wep wlan_tkip wlan_scan_sta wlan_ccmp wlan_acl wlan_scan_ap ath_pci ath_rate_sample ath_rate_minstrel wlan ath_hal hostap_pci hostap ieee80211_crypt "
     
 	for mod in $MODLIST
 	do
