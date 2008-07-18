867fbb5a305f0fe021ed1f56a1c3ad66e2275529
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2008-07-19 01:32:30 +0200

    now it runs with 2 receivers

--- measurement/tcp/tcp_stream.sh
+++ measurement/tcp/tcp_stream.sh
@@ -3,6 +3,10 @@
 sleep 10
 
 ifconfig ath0 192.168.1.2 up
+route del default
+route add default gw 192.168.1.1
+echo "1" > /proc/sys/net/ipv4/ip_forward
+
 ping -c 10 192.168.1.1
 ping -c 4 192.168.5.3
 arp -a
