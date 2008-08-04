e7b74c361d91bd2baabb2c85665569a5ffd25fdb
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2008-08-04 16:36:18 +0200

    news

--- measurement/virtual_antenna/tcp_stream.sh
+++ measurement/virtual_antenna/tcp_stream.sh
@@ -7,7 +7,7 @@ route del default
 route add default gw 192.168.1.1
 
 ping -c 10 192.168.1.1
-ping -c 10 -s 997 192.168.1.2
+ping -c 10 -s 990 192.168.1.2
 #ping -c 100 -s 1000 -i 0.2 192.168.1.2
 arp -a
 
