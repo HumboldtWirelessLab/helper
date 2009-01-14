d1742b3b77cf52cc1fd3627361ab7d64e8cc2be7
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2009-01-14 11:39:03 +0100

    add defaultvalues, so that a special-config  is now easier (params which are not in the config are take from defaultvalues)

--- nodes/bin/wlanmodules.sh
+++ nodes/bin/wlanmodules.sh
@@ -2,50 +2,55 @@
 
 case "$1" in
     "install")
-	if [ "x$MODOPTIONS" = "x" ]; then
-	    . ../etc/madwifi/modoptions.default
-	else
-	    if [ -e $MODOPTIONS ]; then
-	    . $MODOPTIONS
-	    else
-		if [ -e ../etc/madwifi/$MODOPTIONS ]; then
-		  . ../etc/madwifi/$MODOPTIONS
-		else
-		    echo "Modoptionsfile $MODOPTIONS doesn't exist ! Use default"
-		    . ../etc/madwifi/modoptions.default
+    	. $DIR/../../nodes/etc/wifi/default
+
+		if [ "x$MODOPTIONS" = "x" ]; then
+			MODOPTIONS=$DEFAULT_RECOMMENDMODOPTIONS
 		fi
-	    fi
-	fi
-	
-	MODLIST="ath_hal.ko wlan.ko ath_rate_sample.ko wlan_acl.ko wlan_ccmp.ko wlan_scan_ap.ko wlan_scan_sta.ko wlan_tkip.ko wlan_wep.ko wlan_xauth.ko"
-	for mod in $MODLIST
-	do
-	    if [ -f ${MODULSDIR}/$mod ]; then
-		echo "insmod $mod"
-		insmod ${MODULSDIR}/$mod
-	    fi
-	done
+		if [ "x$MODOPTIONS" = "x" ]; then
+			. ../etc/madwifi/modoptions.default
+		else
+			if [ -e $MODOPTIONS ]; then
+			. $MODOPTIONS
+			else
+			if [ -e ../etc/madwifi/$MODOPTIONS ]; then
+			  . ../etc/madwifi/$MODOPTIONS
+			else
+				echo "Modoptionsfile $MODOPTIONS doesn't exist ! Use default"
+				. ../etc/madwifi/modoptions.default
+			fi
+			fi
+		fi
+		
+		MODLIST="ath_hal.ko wlan.ko ath_rate_sample.ko wlan_acl.ko wlan_ccmp.ko wlan_scan_ap.ko wlan_scan_sta.ko wlan_tkip.ko wlan_wep.ko wlan_xauth.ko"
+		for mod in $MODLIST
+		do
+			if [ -f ${MODULSDIR}/$mod ]; then
+			echo "insmod $mod"
+			insmod ${MODULSDIR}/$mod
+			fi
+		done
 
-	echo "insmod ath_pci $ATH_PCI"
-	insmod ${MODULSDIR}/ath_pci.ko $ATH_PCI 
-	;;
+		echo "insmod ath_pci $ATH_PCI"
+		insmod ${MODULSDIR}/ath_pci.ko $ATH_PCI 
+		;;
     "uninstall")
-	MODLIST="wlan_xauth wlan_wep wlan_tkip wlan_scan_ap wlan_scan_sta wlan_ccmp wlan_acl ath_pci ath_rate_sample ath_rate_minstrel wlan ath_hal hostap_pci hostap ieee80211_crypt "
-    
-	for mod in $MODLIST
-	do
-	    MOD_EX=`lsmod | grep $mod | wc -l`
+		MODLIST="wlan_xauth wlan_wep wlan_tkip wlan_scan_ap wlan_scan_sta wlan_ccmp wlan_acl ath_pci ath_rate_sample ath_rate_minstrel wlan ath_hal hostap_pci hostap ieee80211_crypt "
+		
+		for mod in $MODLIST
+		do
+			MOD_EX=`lsmod | grep $mod | wc -l`
 
-	    if [ ! $MOD_EX = 0 ]; then
-		echo "rmmod $mod"
-		rmmod $mod
-	    fi
+			if [ ! $MOD_EX = 0 ]; then
+			echo "rmmod $mod"
+			rmmod $mod
+			fi
 
-	done
-	;;
+		done
+		;;
     *)
-	echo "unknown options"
-	;;
+		echo "unknown options"
+		;;
 esac
 
 exit 0
