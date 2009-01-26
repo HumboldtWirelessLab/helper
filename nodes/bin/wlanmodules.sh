037879125e9f7df03ed82c2396efa3fc1e9afdbe
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2009-01-26 13:02:48 +0100

    add DIR-detection

--- nodes/bin/wlanmodules.sh
+++ nodes/bin/wlanmodules.sh
@@ -1,5 +1,23 @@
 #!/bin/sh
 
+dir=$(dirname "$0")
+pwd=$(pwd)
+
+SIGN=`echo $dir | cut -b 1`
+
+case "$SIGN" in
+    "/")
+        DIR=$dir
+	;;
+    ".")
+        DIR=$pwd/$dir
+        ;;
+    *)
+        echo "Error while getting directory"
+        exit -1
+        ;;
+esac
+
 case "$1" in
     "install")
     	. $DIR/../../nodes/etc/wifi/default
