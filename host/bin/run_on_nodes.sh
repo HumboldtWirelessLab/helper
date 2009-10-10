4b2581ae9c5fcf1a528ee5b4ba4bc96e7d7fb57c
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2009-10-10 23:12:28 +0200

    new scripts

---
+++ host/bin/run_on_nodes.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+dir=$(dirname "$0")
+pwd=$(pwd)
+
+SIGN=`echo $dir | cut -b 1`
+
+case "$SIGN" in
+  "/")
+	DIR=$dir
+        ;;
+  ".")
+	DIR=$pwd/$dir
+	;;
+   *)
+	echo "Error while getting directory"
+	exit -1
+	;;
+esac
+
+. $DIR/functions.sh
+
+for node in $NODELIST; do
+  run_on_node $node "$1" "." $DIR/../etc/keys/id_dsa
+done
