51fe91e05d057a9457b421aebe8840be01c33cad
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2009-10-10 23:12:56 +0200

    layout

--- host/bin/click.sh
+++ host/bin/click.sh
@@ -30,7 +30,7 @@ case "$1" in
 		for node in $NODELIST; do
 		    echo "$node"
 		    if [ !  "x$MODULSDIR" = "x" ]; then
-			run_on_node $node "MODULSDIR=$MODULSDIR ./click.sh install" "$DIR/../../nodes/bin/" $DIR/../etc/keys/id_dsa
+			    run_on_node $node "MODULSDIR=$MODULSDIR ./click.sh install" "$DIR/../../nodes/bin/" $DIR/../etc/keys/id_dsa
 		    fi
 		done
 		;;
