ce9cecf58d7db3d10900d92f363c73183ada7a25
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2009-10-25 22:34:49 +0100

    update

--- nodes/bin/wlandevice.sh
+++ nodes/bin/wlandevice.sh
@@ -228,7 +228,13 @@ case "$1" in
 	    ${IFCONFIG} $DEVICE up
 	    ;;	    
     "getmac")
-      MADDR=`$IFCONFIG $DEVICE | grep HWaddr | awk '{print $5}' | sed -e "s#-# #g" -e "s#:# #g" | awk '{print $1":"$2":"$3":"$4":"$5":"$6}'`
+      MADDRRAW=`$IFCONFIG $DEVICE | grep HWaddr | awk '{print $5}' | sed -e "s#-# #g" -e "s#:# #g"`
+      MADWIFI=`echo $MADDRRAW | awk '{print $1}'`
+      if [ "x$MADWIFI" = "x06" ]; then
+        MADDR=`echo $MADDRRAW | awk '{print $1":"$2":"$3":"$4":"$5":"$6}'`
+      else
+        MADDR=`echo $MADDRRAW | awk '{print "00:"$2":"$3":"$4":"$5":"$6}'`
+      fi
       echo $MADDR
       ;;
     *)
