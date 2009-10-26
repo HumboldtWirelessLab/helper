b3eb9edbb38cf26de5559d4672c9ad5586c44016
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2009-10-26 13:12:42 +0100

    changes

--- measurement/scenarios/simple_flow/simplectrl.sh
+++ measurement/scenarios/simple_flow/simplectrl.sh
@@ -26,10 +26,23 @@ case "$1" in
         ;;
     start)
         echo "Start crtl"
-        sleep 10;
+        sleep 10
         for n in $NODELIST; do
+	  echo "CONNECT TO $n  ###############################"
           clickctrl.sh read $n 7777 lt links
         done
+	SRCMAC=`cat $FINALRESULTDIR/nodelist | grep sk110 | awk '{print $3}'`
+	DSTMAC=`cat $FINALRESULTDIR/nodelist | grep sk111 | awk '{print $3}'`
+	  echo "CONNECT TO sk110  ###############################"
+	clickctrl.sh write sk110 7777 sf add_flow \"$SRCMAC $DSTMAC 1000 100 0 2000 1\"
+	sleep 2
+        echo "CONNECT TO sk110  ###############################"
+	clickctrl.sh write sk110 7777 sf active 0
+	clickctrl.sh read sk110 7777 sf txflows
+	clickctrl.sh read sk110 7777 sf rxflows
+	echo "CONNECT TO sk111  ###############################"
+	clickctrl.sh read sk111 7777 sf txflows
+	clickctrl.sh read sk111 7777 sf rxflows
         ;;
     stop)
         echo "Stop ctrl"
