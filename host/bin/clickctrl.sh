93c9c9d584a46cc53da35183a8cb9c155229c72b
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2009-10-30 17:11:13 +0100

    fix for clickctrl

--- host/bin/clickctrl.sh
+++ host/bin/clickctrl.sh
@@ -26,15 +26,14 @@ if [ "x$1" = "xwrite" ]; then
   ELEMENT=$4
   HANDLER=$5
   shift 5
-  echo "write $ELEMENT.$HANDLER $@" | $NETCAT
+  echo "write $ELEMENT.$HANDLER $@" | $NETCAT | sed '1,3d'
 else
- if [ "x$1" = "xread" ]; then
-   echo "read $4.$5" | $NETCAT
- else
-   echo "Unknown commend"
-   exit 0
- fi
- 
+  if [ "x$1" = "xread" ]; then
+    echo "read $4.$5" | $NETCAT | sed '1,3d'
+  else
+    echo "Unknown commend"
+    exit 0
+  fi
 fi
 
 #java -jar $DIR/JClickClient.jar $@
