d46a104e4fcea134e04832a4a6f14d3840aeab59
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2010-08-26 17:42:32 +0200

    new measurement

--- measurement/scenarios/t-system/005_active_voip/receiver.click
+++ measurement/scenarios/t-system/005_active_voip/receiver.click
@@ -1,7 +1,7 @@
 BRNAddressInfo(my_wlan NODEDEVICE:eth);
 
 FROMRAWDEVICE
-//-> tdraw :: ToDump("RESULTDIR/NODENAME.NODEDEVICE.dump");
+  //-> tdraw :: ToDump("RESULTDIR/NODENAME.NODEDEVICE.dump");
   -> tdraw :: ToDump("/tmp/extra/voip/NODENAME.NODEDEVICE.dump");
   
 
--- measurement/scenarios/t-system/005_active_voip/sync_dumps.sh
+++ measurement/scenarios/t-system/005_active_voip/sync_dumps.sh
@@ -20,6 +20,9 @@ esac
 
 case "$1" in
     start)
+	SYNCPID=`ssh 192.168.4.117 "pidof send_sync"`
+	echo "Stop old Sync"
+	ssh 192.168.4.117 "kill -9 $SYNCPID"
         echo "Start Sync"
 	scp $DIR/send_sync 192.168.4.117:/tmp
 	ssh 192.168.4.117 "/tmp/send_sync 60000 192.168.3.100 30 &"
