106d7cccaa7fe84bd831608305a03fdae4b65819
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2010-09-21 15:55:11 +0200

    preselection mesurement updates (gps,...)

--- measurement/scenarios/t-system/000-preselection/checkdump.sh
+++ measurement/scenarios/t-system/000-preselection/checkdump.sh
@@ -25,32 +25,27 @@ case "$1" in
     echo "Testdump"
     if [ -e $RESULTDIR/devel.ath0.dump ]; then
       echo "Dumpfile ath0 exist"
-    else
-      echo "No ath0 Dumpfile"
-      exit 0;
-    fi
-    if [ -e $RESULTDIR/devel.wlan1.dump ]; then
-      echo "Dumpfile wlan1 exist"
-    else
-      echo "No wlan1 Dumpfile"
-      exit 0;
-    fi
-
     DUMPSIZE=`ls -lisa $RESULTDIR/devel.ath0.dump | awk '{print $7}'`
     if [ $DUMPSIZE -gt 0 ]; then
        echo "Dumpfile ath0 size ok."
      else
        echo "Dumpfile ath0 too small"
-       exit 0;
+    fi
+    else
+      echo "No ath0 Dumpfile"
+    fi
+    if [ -e $RESULTDIR/devel.ath1.dump ]; then
+      echo "Dumpfile wlan1 exist"
+      DUMPSIZE=`ls -lisa $RESULTDIR/devel.ath1.dump | awk '{print $7}'`
+      if [ $DUMPSIZE -gt 0 ]; then
+         echo "Dumpfile ath1 size ok."
+       else
+         echo "Dumpfile ath1 too small"
+       fi
+    else
+      echo "No wlan1 Dumpfile"
     fi
 
-    DUMPSIZE=`ls -lisa $RESULTDIR/devel.wlan1.dump | awk '{print $7}'`
-    if [ $DUMPSIZE -gt 0 ]; then
-       echo "Dumpfile wlan1 size ok."
-     else
-       echo "Dumpfile wlan1 too small"
-       exit 0;
-    fi
 
     (cd $RESULTDIR; /home/testbed/click-brn/userlevel/click $DIR/checkdump.click > testresult.log 2>&1)
     
