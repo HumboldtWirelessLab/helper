99c735aae3eabb5e7042160f7a4fdcf8ec3dcec7
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2010-09-27 13:59:15 +0200

    faster click-ctrl

--- evaluation/scenarios/standard/eval.sh
+++ evaluation/scenarios/standard/eval.sh
@@ -32,7 +32,7 @@ OUTNODES=""
 for od in $OUTDUMPS; do
   OUTNODE=`echo $od | sed "s#\.# #g" | awk '{print $1}'`
   OUTDEVICE=`echo $od | sed "s#\.# #g" | awk '{print $2}'`
-  OUTMACS=`cat $od | grep "Sequence" | awk '{print $6$7}' | cut -b 5-16  | sort -u`
+  OUTMACS=`cat $od | grep "Sequence" | awk '{print $10$11}' | cut -b 5-16  | sort -u`
 
   for om in $OUTMACS; do
     FORMATEDMAC=`echo $om | awk '{print substr($1,1,2)"-"substr($1,3,2)"-"substr($1,5,2)"-"substr($1,7,2)"-"substr($1,9,2)"-"substr($1,11,2)}'`
@@ -40,7 +40,7 @@ for od in $OUTDUMPS; do
     echo "$OUTNODE $OUTDEVICE $FORMATEDMAC" >> outmacs.dat
   done
 
-  NEWOUTNODE=`cat $od | grep "Sequence" | awk '{print $5" "$6" "$7}' | sort -u`
+  NEWOUTNODE=`cat $od | grep "Sequence" | awk '{print $9" "$10" "$11}' | sort -u`
   OUTNODES="$OUTNODES $NEWOUTNODE"
 done
 
@@ -72,17 +72,17 @@ for dump in `ls *.dump.all.dat`; do
 	
 	BASEFILE=`echo $dump | sed "s#.all.dat##g"`
 	
-	cat $dump | grep "OKPacket:" | awk '{ print $2 "\t" $3 "\t" $5 "\t" $8}' | sed -s 's/://g' | sed -s 's/Mb//g' | sed -s 's/mgmt/0/g' | sed -s 's/cntl/1/g' | sed -s 's/data/2/g' > $BASEFILE.ok.dat
-	cat $dump | grep "CRCerror:" | awk '{ print $2 "\t" $3 "\t" $5 }' | sed -s 's/://g' | sed -s 's/Mb//g' > $BASEFILE.crc.dat
-	cat $dump | grep "PHYerror:" | awk '{ print $2 "\t" $3 }' | sed -s 's/://g' | sed -s 's/Mb//g' >  $BASEFILE.phy.dat
-	cat $dump | grep "OKPacket:" | grep "mgmt beacon\|mgmt probe_resp" | awk '{ print $10 }' | sort -u  >  $BASEFILE.bssid.dat
-	cat $dump | grep "OKPacket:" | grep "mgmt beacon\|mgmt probe_resp" | awk '{ print $10 }' | sort -u  | wc -l >> all_bssid.dat
+	cat $dump | grep "OKPacket:" | awk '{ print $6 "\t" $7 "\t" $9 "\t" $12}' | sed -s 's/://g' | sed -s 's/Mb//g' | sed -s 's/mgmt/0/g' | sed -s 's/cntl/1/g' | sed -s 's/data/2/g' > $BASEFILE.ok.dat
+	cat $dump | grep "CRCerror:" | awk '{ print $6 "\t" $7 "\t" $9 }' | sed -s 's/://g' | sed -s 's/Mb//g' > $BASEFILE.crc.dat
+	cat $dump | grep "PHYerror:" | awk '{ print $6 "\t" $7 }' | sed -s 's/://g' | sed -s 's/Mb//g' >  $BASEFILE.phy.dat
+	cat $dump | grep "OKPacket:" | grep "mgmt beacon\|mgmt probe_resp" | awk '{ print $14 }' | sort -u  >  $BASEFILE.bssid.dat
+	cat $dump | grep "OKPacket:" | grep "mgmt beacon\|mgmt probe_resp" | awk '{ print $14 }' | sort -u  | wc -l >> all_bssid.dat
 	
 	if [ "x$SINGLEOUTMAC" != "x" ]; then
-          cat $dump | grep "OKPacket:" | grep -i $SINGLEOUTMAC | awk '{ print $6 }' | sed -s 's/+//g' | awk -F "/" '{ print $1 }' >  $BASEFILE.rssi_ref.dat
+          cat $dump | grep "OKPacket:" | grep -i $SINGLEOUTMAC | awk '{ print $10 }' | sed -s 's/+//g' | awk -F "/" '{ print $1 }' >  $BASEFILE.rssi_ref.dat
  	fi
 	  
-	cat $dump | grep "Sequence:" | awk '{ print $2"\tSRCMAC" $6$7 "\t" $9 }' | sed "s#SRCMACffff##g" | sed "s#:##g" >>  $BASEFILE.seq_no.dat.tmp
+	cat $dump | grep "Sequence:" | awk '{ print $6"\tSRCMAC" $10$11 "\t" $13 }' | sed "s#SRCMACffff##g" | sed "s#:##g" >>  $BASEFILE.seq_no.dat.tmp
 	cat $BASEFILE.seq_no.dat.tmp | awk '{print $2" "$3}'>> all_seq_no.dat.tmp
         cat $BASEFILE.seq_no.dat.tmp | awk '{print $1"\t"$2"\t"strtonum("0x"$3)}' >> $BASEFILE.seq_no.dat
 	rm $BASEFILE.seq_no.dat.tmp
