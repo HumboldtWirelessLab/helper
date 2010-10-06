3d63df74520a600902c8c2fc5568285f12c62d66
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2010-10-06 18:24:30 +0200

    add mobile_preselection

---
+++ measurement/scenarios/t-system/000_mobile_preselection/sync_dumps.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+dir=$(dirname "$0")
+pwd=$(pwd)
+
+SIGN=`echo $dir | cut -b 1`
+
+case "$SIGN" in
+  "/")
+      DIR=$dir
+      ;;
+  ".")
+      DIR=$pwd/$dir
+      ;;
+   *)
+      echo "Error while getting directory"
+      exit -1
+      ;;
+esac
+
+case "$1" in
+    start)
+	SYNCPID=`pidof send_sync`
+	echo "Stop old Sync"
+	kill -9 $SYNCPID
+        echo "Start Sync"
+	$DIR/send_sync 60000 192.168.3.2 30 &
+	;;
+    stop)
+	SYNCPID=`pidof send_sync`
+	echo "Stop Sync"
+	kill -9 $SYNCPID
+	;;
+    *)
+	echo "Use $0 start|stop"
+	;;
+esac
+
+exit 0
