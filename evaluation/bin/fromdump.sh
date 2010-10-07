f62b3b8764b7603625d47aa1008a07d414546024
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2010-10-07 17:01:37 +0200

    support for old gps

--- evaluation/bin/fromdump.sh
+++ evaluation/bin/fromdump.sh
@@ -30,14 +30,22 @@ else
   SEQREP="//"
 fi
 
-if [ "x$GPS" = "xyes" ]; then
+GPSDECAP="GPSDecap()"
+GPSPRINT="GPSPrint(NOWRAP true)"
+
+if [ "x$GPS" = "xyes" ] || [ "x$GPS" = "xold" ]; then
   GPSREP=""
+  if [ "x$GPS" = "xold" ]; then
+    GPSDECAP="Strip(12)"
+    GPSPRINT="GPSPrint\(NOWRAP true,OLDGPS true\)"
+  fi
 else
   GPSREP="//"
 fi
+
 
 if [ "x$WIFI" = "x" ]; then
   WIFI=805
 fi
 
-cat $DIR/../etc/click/eval_wifi_$WIFI.click | sed "s#DUMP#$1#g" | sed "s#//SEQ#$SEQREP#g" | sed "s#//ATH#$ATHREP#g" | sed "s#//GPS#$GPSREP#g" | click-align 2> /dev/null | click 2>&1
+cat $DIR/../etc/click/eval_wifi_$WIFI.click | sed "s#DUMP#$1#g" | sed "s#//SEQ#$SEQREP#g" | sed "s#//ATH#$ATHREP#g" | sed "s#//GPS#$GPSREP#g" | sed "s#GPSDecap()#$GPSDECAP#g" | sed "s#GPSPrint(NOWRAP true)#$GPSPRINT#g" | click-align 2> /dev/null | click 2>&1
