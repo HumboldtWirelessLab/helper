0946b48e21002bb75c35daa57f0e25e5f3e2b9c8
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2010-10-14 17:22:47 +0200

    updates

--- measurement/scenarios/t-system/005_active_voip/ft.sh
+++ measurement/scenarios/t-system/005_active_voip/ft.sh
@@ -41,87 +41,91 @@ fi
 
 mv foreign_node.click foreign_node.click.save
 
-echo "No foreign traffic"
-./growing_measurement.sh 1 $MAXNODES 1 $DURATION $STARTTIME $MIDDURATION $ENDDURATION
+if [ ! -e $MPREFIX-noft ]; then
+    echo "No foreign traffic"
+    ./growing_measurement.sh 1 $MAXNODES 1 $DURATION $STARTTIME $MIDDURATION $ENDDURATION
+    mv 1 $MPREFIX-noft
+fi
 
-mv 1 $MPREFIX-noft
 
-
-
-echo "Foreign traffic 10% with cca"
 
 cat foreign_node.click.save | sed "s#SIZE 70#SIZE 93#g" | sed "s#INTERVAL 12#INTERVAL 14#g" | sed "s#BURST 4#BURST 1#g" > foreign_node.click
-FT=yes CCA=1 ./growing_measurement.sh 1 $MAXNODES 1 $DURATION $STARTTIME $MIDDURATION $ENDDURATION
 
-mv 1 $MPREFIX-10ft_cca
+if [ ! -e $MPREFIX-10ft_cca ]; then
+  echo "Foreign traffic 10% with cca"
+  FT=yes CCA=1 ./growing_measurement.sh 1 $MAXNODES 1 $DURATION $STARTTIME $MIDDURATION $ENDDURATION
+  mv 1 $MPREFIX-10ft_cca
+fi
 
-
-echo "Foreign traffic 10% with nocca"
-FT=yes CCA=0 ./growing_measurement.sh 1 $MAXNODES 1 $DURATION $STARTTIME $MIDDURATION $ENDDURATION
-
-mv 1 $MPREFIX-10ft_nocca
-
-
-
-
-echo "Foreign traffic 20% with cca"
-
-cat foreign_node.click.save | sed "s#SIZE 70#SIZE 218#g" | sed "s#INTERVAL 12#INTERVAL 10#g" | sed "s#BURST 4#BURST 1#g" > foreign_node.click
-FT=yes CCA=1 ./growing_measurement.sh 1 $MAXNODES 1 $DURATION $STARTTIME $MIDDURATION $ENDDURATION
-
-mv 1 $MPREFIX-20ft_cca
-
-echo "Foreign traffic 20% with nocca"
-FT=yes CCA=0 ./growing_measurement.sh 1 $MAXNODES 1 $DURATION $STARTTIME $MIDDURATION $ENDDURATION
-
-mv 1 $MPREFIX-20ft_nocca
+if [ ! -e $MPREFIX-10ft_nocca ]; then
+  echo "Foreign traffic 10% with nocca"
+  FT=yes CCA=0 ./growing_measurement.sh 1 $MAXNODES 1 $DURATION $STARTTIME $MIDDURATION $ENDDURATION
+  mv 1 $MPREFIX-10ft_nocca
+fi
 
 
 
+#cat foreign_node.click.save | sed "s#SIZE 70#SIZE 218#g" | sed "s#INTERVAL 12#INTERVAL 10#g" | sed "s#BURST 4#BURST 1#g" > foreign_node.click
 
-echo "Foreign traffic 30% with cca"
+#if [ ! -e $MPREFIX-20ft_cca ]; then
+#  echo "Foreign traffic 20% with cca"
+#  FT=yes CCA=1 ./growing_measurement.sh 1 $MAXNODES 1 $DURATION $STARTTIME $MIDDURATION $ENDDURATION
+#  mv 1 $MPREFIX-20ft_cca
+#fi
+
+#if [ ! -e $MPREFIX-20ft_nocca ]; then
+#  echo "Foreign traffic 20% with nocca"
+#  FT=yes CCA=0 ./growing_measurement.sh 1 $MAXNODES 1 $DURATION $STARTTIME $MIDDURATION $ENDDURATION
+#  mv 1 $MPREFIX-20ft_nocca
+#fi
+
+
 
 cat foreign_node.click.save | sed "s#SIZE 70#SIZE 343#g" | sed "s#INTERVAL 12#INTERVAL 10#g" | sed "s#BURST 4#BURST 1#g" > foreign_node.click
-FT=yes CCA=1 ./growing_measurement.sh 1 $MAXNODES 1 $DURATION $STARTTIME $MIDDURATION $ENDDURATION
 
-mv 1 $MPREFIX-30ft_cca
+if [ ! -e $MPREFIX-30ft_cca ]; then
+  echo "Foreign traffic 30% with cca"
+  FT=yes CCA=1 ./growing_measurement.sh 1 $MAXNODES 1 $DURATION $STARTTIME $MIDDURATION $ENDDURATION
+  mv 1 $MPREFIX-30ft_cca
+fi
 
-echo "Foreign traffic 30% with nocca"
-FT=yes CCA=0 ./growing_measurement.sh 1 $MAXNODES 1 $DURATION $STARTTIME $MIDDURATION $ENDDURATION
-
-mv 1 $MPREFIX-30ft_nocca
-
-
-
-
-echo "Foreign traffic 40% with cca"
-
-cat foreign_node.click.save | sed "s#SIZE 70#SIZE 468#g" | sed "s#INTERVAL 12#INTERVAL 10#g" | sed "s#BURST 4#BURST 1#g" > foreign_node.click
-FT=yes CCA=1 ./growing_measurement.sh 1 $MAXNODES 1 $DURATION $STARTTIME $MIDDURATION $ENDDURATION
-
-mv 1 $MPREFIX-40ft_cca
-
-echo "Foreign traffic 40% with nocca"
-FT=yes CCA=0 ./growing_measurement.sh 1 $MAXNODES 1 $DURATION $STARTTIME $MIDDURATION $ENDDURATION
-
-mv 1 $MPREFIX-40ft_nocca
+if [ ! -e $MPREFIX-30ft_nocca ]; then
+  echo "Foreign traffic 30% with nocca"
+  FT=yes CCA=0 ./growing_measurement.sh 1 $MAXNODES 1 $DURATION $STARTTIME $MIDDURATION $ENDDURATION
+  mv 1 $MPREFIX-30ft_nocca
+fi
 
 
 
+#cat foreign_node.click.save | sed "s#SIZE 70#SIZE 468#g" | sed "s#INTERVAL 12#INTERVAL 10#g" | sed "s#BURST 4#BURST 1#g" > foreign_node.click
 
-echo "Foreign traffic 50% with cca"
+#if [ ! -e $MPREFIX-40ft_cca ]; then
+#  echo "Foreign traffic 40% with cca"
+#  FT=yes CCA=1 ./growing_measurement.sh 1 $MAXNODES 1 $DURATION $STARTTIME $MIDDURATION $ENDDURATION
+#  mv 1 $MPREFIX-40ft_cca
+#fi
+
+#if [ ! -e $MPREFIX-40ft_nocca ]; then
+#  echo "Foreign traffic 40% with nocca"
+#  FT=yes CCA=0 ./growing_measurement.sh 1 $MAXNODES 1 $DURATION $STARTTIME $MIDDURATION $ENDDURATION
+#  mv 1 $MPREFIX-40ft_nocca
+#fi
+
+
 
 cat foreign_node.click.save | sed "s#SIZE 70#SIZE 593#g" | sed "s#INTERVAL 12#INTERVAL 10#g" | sed "s#BURST 4#BURST 1#g" > foreign_node.click
-FT=yes CCA=1 ./growing_measurement.sh 1 $MAXNODES 1 $DURATION $STARTTIME $MIDDURATION $ENDDURATION
 
-mv 1 $MPREFIX-50ft_cca
+if [ ! -e $MPREFIX-50ft_cca ]; then
+  echo "Foreign traffic 50% with cca"
+  FT=yes CCA=1 ./growing_measurement.sh 1 $MAXNODES 1 $DURATION $STARTTIME $MIDDURATION $ENDDURATION
+  mv 1 $MPREFIX-50ft_cca
+fi
 
-echo "Foreign traffic 50% with nocca"
-FT=yes CCA=0 ./growing_measurement.sh 1 $MAXNODES 1 $DURATION $STARTTIME $MIDDURATION $ENDDURATION
-
-mv 1 $MPREFIX-50ft_nocca
-
-
+if [ ! -e $MPREFIX-50ft_nocca ]; then
+  echo "Foreign traffic 50% with nocca"
+  FT=yes CCA=0 ./growing_measurement.sh 1 $MAXNODES 1 $DURATION $STARTTIME $MIDDURATION $ENDDURATION
+  mv 1 $MPREFIX-50ft_nocca
+fi
 
 
 echo "restore org"
@@ -130,3 +134,4 @@ rm -f foreign_node.click
 mv foreign_node.click.save foreign_node.click
 
 exit 0
+
