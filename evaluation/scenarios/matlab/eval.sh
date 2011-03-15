15f5a36413a06f0e5807a432bb7590db0c614c73
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2011-03-15 11:58:13 +0100

    Update basic evaluation files

--- evaluation/scenarios/matlab/eval.sh
+++ evaluation/scenarios/matlab/eval.sh
@@ -1,3 +1,22 @@
+#!/bin/sh
+
+dir=$(dirname "$0")
+pwd=$(pwd)
+
+SIGN=`echo $dir | cut -b 1`
+
+case "$SIGN" in
+  "/")
+      DIR=$dir
+      ;;
+  ".")
+      DIR=$pwd/$dir
+      ;;
+    *)
+      echo "Error while getting directory"
+      exit -1
+      ;;
+esac
 
 for d in `(ls *.dump.all.dat)`; do
 
@@ -10,6 +29,6 @@ PREFIX=`echo $d | sed "s#\.dump\.all\.dat##g"`
 # PREFIX.data.dat			: contains all the data formated to integer values
 # PREFIX.ignoredlines.log	: contains ignored lines like TX, ATHOPERATION or faulty formats.
 # 	Further Infos see the awk script itself. 
-cat $d | awk -vPREFIX=$PREFIX -f genMatlabFile.awk
+cat $d | awk -vPREFIX=$PREFIX -f $DIR/genMatlabFile.awk
 
 done
