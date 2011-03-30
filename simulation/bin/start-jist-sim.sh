5c1a4a60941bb6efaeebe1d3ffe98d10fb88d498
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2011-03-30 19:30:28 +0200

    Move JistSimulation

--- simulation/bin/start-jist-sim.sh
+++ simulation/bin/start-jist-sim.sh
@@ -7,4 +7,4 @@ export JISTCOMMON_HOME=$BRN_TOOLS_PATH/brn.sim/brn.jist
 export JISTCLICK_HOME=$BRN_TOOLS_PATH/brn.sim/brn.jist.click
 export JIST_HOME=$BRN_TOOLS_PATH/brn.sim/jist.swans
 
-(cd $JISTCLICK_HOME; ant run -Drun.class=test.brn.mesh.jistsimulation.JistSimulation -Drun.args="$1") | grep "\[java\]" | grep -v "Controller:INFO:" | sed "s#^[[:space:]]*\[java\][[:space:]]##g"
+(cd $JISTCLICK_HOME; ant run -Drun.class=brn.sim.scenario.jistsimulation.JistSimulation -Drun.args="$1") | grep "\[java\]" | grep -v "Controller:INFO:" | sed "s#^[[:space:]]*\[java\][[:space:]]##g"
