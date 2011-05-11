7be648e50fc68c362ae6734d0d8c550cf9e59a6d
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2011-05-11 12:06:10 +0200

    run_click.sh
    Signed-off-by: Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>

---
+++ measurement/bin/run_click.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+dir=$(dirname "$0")
+pwd=$(pwd)
+
+SIGN=`echo $dir | cut -b 1`
+
+case "$SIGN" in
+  "/")
+	DIR=$dir
+        ;;
+  ".")
+	DIR=$pwd/$dir
+	;;
+   *)
+	echo "Error while getting directory"
+	exit -1
+	;;
+esac
+
+cpp -I$DIR/../etc/click/ $1 | grep -v "#" | click
+
