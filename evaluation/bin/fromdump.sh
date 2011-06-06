ee57b41be8bac68a53a600f48a960a01dbbdea11
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2011-06-06 23:55:04 +0200

    print fromdump script

--- evaluation/bin/fromdump.sh
+++ evaluation/bin/fromdump.sh
@@ -52,4 +52,8 @@ if [ "x$WIFI" = "x" ]; then
   WIFI=805
 fi
 
-cat $DIR/../etc/click/eval_wifi_$WIFI.click | sed "s#DUMP#$1#g" | sed "s#//SEQ#$SEQREP#g" | sed "s#//ATH#$ATHREP#g" | sed "s#//GPS#$GPSREP#g" | sed "s#GPSDecap()#$GPSDECAP#g" | sed "s#GPSPrint(NOWRAP true)#$GPSPRINT#g" | click-align 2> /dev/null | click 2>&1
+if [ "x$2" = "xprint" ]; then
+  cat $DIR/../etc/click/eval_wifi_$WIFI.click | sed "s#DUMP#$1#g" | sed "s#//SEQ#$SEQREP#g" | sed "s#//ATH#$ATHREP#g" | sed "s#//GPS#$GPSREP#g" | sed "s#GPSDecap()#$GPSDECAP#g" | sed "s#GPSPrint(NOWRAP true)#$GPSPRINT#g" > ./eval.click
+else
+  cat $DIR/../etc/click/eval_wifi_$WIFI.click | sed "s#DUMP#$1#g" | sed "s#//SEQ#$SEQREP#g" | sed "s#//ATH#$ATHREP#g" | sed "s#//GPS#$GPSREP#g" | sed "s#GPSDecap()#$GPSDECAP#g" | sed "s#GPSPrint(NOWRAP true)#$GPSPRINT#g" | click-align 2> /dev/null | click 2>&1
+fi
