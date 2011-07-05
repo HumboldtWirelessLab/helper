1abbd2e3580b88da6b32e39328b3c6b851f5ac38
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2011-07-05 12:50:35 +0200

    add file for openbeacon

---
+++ nodes/lib/wifidriver/openbeacon.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+
+dir=$(dirname "$0")
+pwd=$(pwd)
+
+SIGN=`echo $dir | cut -b 1`
+
+case "$SIGN" in
+    "/")
+        DIR=$dir
+	;;
+    ".")
+        DIR=$pwd/$dir
+        ;;
+    *)
+        echo "Error while getting directory"
+        exit -1
+        ;;
+esac
+
+case "$1" in
+    "responsible")
+		#check whether ath_pci.ko exists. if so, then his script is responsible 
+                KERNELVERSION=`uname -r`
+                NODEARCH=`$DIR/../../bin/system.sh get_arch`
+		
+                FINMODULSDIR=`echo $MODULSDIR | sed -e "s#KERNELVERSION#$KERNELVERSION#g" -e "s#NODEARCH#$NODEARCH#g"`
+		if [ -f ${FINMODULSDIR}/.o ]; then
+		    exit 0
+		else
+		    exit 1
+		fi
+		;;
+    "device_name")
+                IS_TMPL=`echo $DEVICE | grep DEV | wc -l`
+		    
+	        if [ $IS_TMPL -ne 0 ]; then
+	            NUM=`echo $DEVICE | sed "s#DEV##g"`
+	            echo "obd$NUM"
+	        else
+	            echo $DEVICE
+	        fi
+                ;;																  
+    "install")
+                KERNELVERSION=`uname -r`
+                NODEARCH=`$DIR/../../bin/system.sh get_arch`
+		
+                FINMODULSDIR=`echo $MODULSDIR | sed -e "s#KERNELVERSION#$KERNELVERSION#g" -e "s#NODEARCH#$NODEARCH#g"`
+
+		export PATH=$PATH:/usr/bin:/usr/sbin:/bin:/sbin
+		rmmod usbserial
+		(cd $FINMODULSDIR; insmod ./usbserial.ko vendor=0x03EB product=0x6124; insmod ./cdc_acm.ko)
+		;;
+    "uninstall")
+		;;
+    *)
+		echo "unknown options"
+		;;
+esac
+
+exit 0
