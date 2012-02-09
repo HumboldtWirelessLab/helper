5c86b99bbad5e3af8dbe26ed372b5dc59db47780
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2012-02-09 17:13:24 +0100

    ns2 params for control frames

--- simulation/etc/ns/radio/tworayground11b.tcl
+++ simulation/etc/ns/radio/tworayground11b.tcl
@@ -49,6 +49,7 @@ Mac/802_11 set LongRetryLimit_  4               ;# retransmissions
 Mac/802_11 set TXFeedback_ 1
 Mac/802_11 set Promisc_ 1
 Mac/802_11 set FilterDub_ 0
+Mac/802_11 set ControlFrames_ 1
 
 #
 # The network channel, physical layer, MAC, propagation model,
