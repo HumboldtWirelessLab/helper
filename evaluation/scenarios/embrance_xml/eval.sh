8f5747d6c79811378f69b7df2ad12a05e254076d
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2012-03-15 13:59:02 +0100

    embrance_xml: fix wrong path

--- evaluation/scenarios/embrance_xml/eval.sh
+++ evaluation/scenarios/embrance_xml/eval.sh
@@ -24,7 +24,7 @@ if [ -f $RESULTDIR/measurement.log ]; then
     cat $RESULTDIR/measurement.log | grep -e "^[[:space:]]*<" > $EVALUATIONSDIR/measurement.xml
     cat $RESULTDIR/measurement.log | grep -v "^[[:space:]]*<" > $EVALUATIONSDIR/measurement_debug.log
     echo "<$NAME>" > $EVALUATIONSDIR/measurement.xml.tmp
-    cat measurement.xml >> $EVALUATIONSDIR/measurement.xml.tmp
+    cat $EVALUATIONSDIR/measurement.xml >> $EVALUATIONSDIR/measurement.xml.tmp
     echo "</$NAME>" >> $EVALUATIONSDIR/measurement.xml.tmp
     mv $EVALUATIONSDIR/measurement.xml.tmp $EVALUATIONSDIR/measurement.xml
 fi
