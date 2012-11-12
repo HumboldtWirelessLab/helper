abaa6684125549be00e89a0e2c98b0193834dfd3
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2012-11-12 19:10:15 +0100

    Evaluation: flooding, flow

---
+++ evaluation/scenarios/flooding_info/eval.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+dir=$(dirname "$0")
+pwd=$(pwd)
+
+SIGN=`echo $dir | cut -b 1`
+
+case "$SIGN" in
+  "/")
+        DIR=$dir
+        ;;
+  ".")
+        DIR=$pwd/$dir
+        ;;
+   *)
+        echo "Error while getting directory"
+        exit -1
+        ;;
+esac
+
+. $CONFIGFILE
+
+if [ ! -e $EVALUATIONSDIR ]; then
+  mkdir -p $EVALUATIONSDIR
+fi
+
+if [ -f $RESULTDIR/measurement.xml ]; then
+  DATAFILE=$RESULTDIR/measurement.xml
+else
+  if [ -f $EVALUATIONSDIR/measurement.xml ]; then
+    DATAFILE=$EVALUATIONSDIR/measurement.xml
+  else
+    DATAFILE=$RESULTDIR/measurement.log
+  fi
+fi
+
+FULLSED=""
+while read line; do
+  SRCN=`echo $line | awk '{print $1}'` 
+  SRCM=`echo $line | awk '{print $3}'`
+
+  FULLSED="$FULLSED -e s#$SRCM#$SRCN#g"
+done < $RESULTDIR/nodes.mac
+
+xsltproc $DIR/flooding.xslt $DATAFILE > $EVALUATIONSDIR/floodingstats.csv
---
+++ evaluation/scenarios/flow_info/flowstats_tx.xslt
@@ -0,0 +1,20 @@
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+
+    <xsl:output method='text'/>
+
+    <xsl:variable name='newline'><xsl:text>
+</xsl:text></xsl:variable>
+
+    <xsl:template match="text( )|@*">
+      <!--<xsl:value-of select="."/>-->
+    </xsl:template>
+
+    <xsl:template match="txflow">
+	<xsl:value-of select="../@node" /><xsl:text>,</xsl:text>
+	<xsl:value-of select="@src" /><xsl:text>,</xsl:text>
+	<xsl:value-of select="@dst" /><xsl:text>,</xsl:text>
+	<xsl:value-of select="@avg_hops" /><xsl:text>,</xsl:text>
+	<xsl:value-of select="@packet_count" /><xsl:text>,</xsl:text>
+	<xsl:value-of select="@packet_count" /><xsl:value-of select="$newline" />
+    </xsl:template>
+</xsl:stylesheet>
