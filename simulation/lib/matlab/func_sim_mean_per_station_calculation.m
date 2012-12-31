5a057b58d6d98a06889a4c7ac305eced07be25e0
Boris Schroeder <bschroed@informatik.hu-berlin.de>
2012-12-31 17:36:56 +0100

    Matlab-Simulator modifiziert

--- simulation/lib/matlab/func_sim_mean_per_station_calculation.m
+++ simulation/lib/matlab/func_sim_mean_per_station_calculation.m
@@ -1,9 +1,13 @@
-function [matrix_likelihood_collisions] = func_sim_mean_per_station_calculation(matrix, matrix_packets_successful_delivered)
+function [matrix_likelihood_collisions] = func_sim_mean_per_station_calculation(matrix, matrix_packets_successful_delivered,read_3D_on)
     matrix_likelihood_collisions = zeros(size(matrix,1),size(matrix,2),size(matrix,3));
     for p=1:1:size(matrix,3)
         for t=1:1:size(matrix,2)
             for z=1:1:size(matrix,1)
-                matrix_likelihood_collisions(z,t,p) = matrix(z,t,p) / (matrix(z,t,p) + matrix_packets_successful_delivered(p,t));
+                if (read_3D_on == 1)
+                    matrix_likelihood_collisions(z,t,p) = matrix(z,t,p) / (matrix(z,t,p) + matrix_packets_successful_delivered(z,t,p));
+                else
+                    matrix_likelihood_collisions(z,t,p) = matrix(z,t,p) / (matrix(z,t,p) + matrix_packets_successful_delivered(p,t));
+                end
             end
         end
     end
