13e345fa46165b914626d79cf73b21b7ac95047b
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2013-11-24 01:10:56 +0100

    RawDev: fix error Eval: add config_compression Flooding: reduce e2e delay

---
+++ evaluation/scenarios/compression_config/eval.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+dir=$(dirname "$0")
+pwd=$(pwd)
+
+SIGN=`echo $dir | cut -b 1`
+
+case "$SIGN" in
+  "/")
+	DIR=$dir
+        ;;
+  ".")
+	DIR=$pwd/$dir
+	;;
+   *)
+	echo "Error while getting directory"
+	exit -1
+	;;
+esac
+
+. $CONFIGFILE
+
+(cd $RESULTDIR; tar cfj $NAME\_click_scripts.tar.bz2 `cat $NODETABLE | awk '{print $7}' | sed "s#$RESULTDIR/##g" | grep -v -e "^-$"`; cat $NODETABLE | awk '{print $7}' | grep -v -e "^-$" | xargs rm -rf )
+
+exit 0
+
