33abf10f8ee9a2263bb4e8d2dc22e647139437ed
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2014-04-08 17:47:17 +0200

    Ns2: Set nodecount for shadowing cache

--- simulation/etc/ns/radio/shadowing.tcl
+++ simulation/etc/ns/radio/shadowing.tcl
@@ -1,9 +1,10 @@
 # Propagation model
 # first set values of shadowing model
-Propagation/Shadowing set pathlossExp_ 2.0  ;# path loss exponent
-Propagation/Shadowing set std_db_ 2.0       ;# shadowing deviation (dB)
-Propagation/Shadowing set dist0_ 1.0        ;# reference distance (m)
-Propagation/Shadowing set seed_ 0           ;# seed for RNG
+Propagation/Shadowing set pathlossExp_ 2.0    ;# path loss exponent
+Propagation/Shadowing set std_db_ 2.0         ;# shadowing deviation (dB)
+Propagation/Shadowing set dist0_ 1.0          ;# reference distance (m)
+Propagation/Shadowing set seed_ 0             ;# seed for RNG
+Propagation/Shadowing set nonodes_ $nodecount ;# set node count for shadowing cache
 
 #Propagation/Shadowing set pathlossExp_ 3.0  ;# path loss exponent
 #Propagation/Shadowing set std_db_ 5.0       ;# shadowing deviation (dB)
