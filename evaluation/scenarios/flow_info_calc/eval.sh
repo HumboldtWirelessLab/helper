8c25faaa3660ed02d5f3673b3c141464e9000046
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2014-08-07 17:15:00 +0200

    Eval flow_info: depends

--- evaluation/scenarios/flow_info_calc/eval.sh
+++ evaluation/scenarios/flow_info_calc/eval.sh
@@ -24,21 +24,6 @@ if [ ! -e $EVALUATIONSDIR ]; then
   mkdir -p $EVALUATIONSDIR
 fi
 
-if [ -f $RESULTDIR/measurement.xml ]; then
-  DATAFILE=$RESULTDIR/measurement.xml
-else
-  if [ -f $EVALUATIONSDIR/measurement.xml ]; then
-    DATAFILE=$EVALUATIONSDIR/measurement.xml
-  else
-    DATAFILE=$RESULTDIR/measurement.log
-  fi
-fi
-
 EVALUATIONSDIR="$EVALUATIONSDIR""/flow_info"
-
-#NONODES=`cat $RESULTDIR/nodes.mac | wc -l`
-#RXCOUNT=`cat $EVALUATIONSDIR/flowstats_rx.csv | wc -l`
-#TXBCASTCOUNT=`cat $EVALUATIONSDIR/flowstats_tx.csv | grep "FF-FF-FF-FF-FF-FF" | wc -l`
-#TXUNICASTCOUNT=`cat $EVALUATIONSDIR/flowstats_tx.csv | grep -v "FF-FF-FF-FF-FF-FF" | wc -l`
 
 (cd $DIR; matwrapper.sh "flowstats('$EVALUATIONSDIR/flowstats_rx.mat','$EVALUATIONSDIR/flowtime.mat')")
