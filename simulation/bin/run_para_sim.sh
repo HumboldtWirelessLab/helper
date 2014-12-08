76971d7c972a4d221b7bd1d8002f1d196c6e1f99
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2014-12-08 16:57:50 +0100

    run_sim/measurement: use eval_again.sh; use para_sim for sim_params

--- simulation/bin/run_para_sim.sh
+++ simulation/bin/run_para_sim.sh
@@ -64,14 +64,14 @@ for i in `find -name *.des.sim`; do
 
   #echo $SIMDIR
   if [ $USE_DISTPARASIM -eq 1 ]; then
-    JOBCOMMAND="cd $WORKINGDIR/$SIMDIR/;run_again.sh > run_again.log 2>&1; sh ./eval_again.sh > eval_again.log 2>&1; touch $WORKINGDIR/sim_finish_dir/$NUM" $DIR/../lib/parasim/para_sim_ctrl.sh commit
+    JOBCOMMAND="cd $WORKINGDIR/$SIMDIR/;run_again.sh > run_again.log 2>&1; eval_again.sh > eval_again.log 2>&1; touch $WORKINGDIR/sim_finish_dir/$NUM" $DIR/../lib/parasim/para_sim_ctrl.sh commit
   else
     if [ $CONT -eq 0 ]; then
-      (cd $SIMDIR/;run_again.sh > run_again.log 2>&1; sh ./eval_again.sh > eval_again.log 2>&1; cd $WORKINGDIR; touch $WORKINGDIR/sim_finish_dir/$NUM ) &
+      (cd $SIMDIR/;run_again.sh > run_again.log 2>&1; eval_again.sh > eval_again.log 2>&1; cd $WORKINGDIR; touch $WORKINGDIR/sim_finish_dir/$NUM ) &
     else
       DONE=`grep "^$NUM\$" $WORKINGDIR/sim_finish | wc -l`
       if [ $DONE -eq 0 ]; then
-        (cd $SIMDIR/;run_again.sh > run_again.log 2>&1; sh ./eval_again.sh > eval_again.log 2>&1; cd $WORKINGDIR; touch $WORKINGDIR/sim_finish_dir/$NUM ) &
+        (cd $SIMDIR/;run_again.sh > run_again.log 2>&1; eval_again.sh > eval_again.log 2>&1; cd $WORKINGDIR; touch $WORKINGDIR/sim_finish_dir/$NUM ) &
       else
         touch $WORKINGDIR/sim_finish_dir/$NUM
       fi
