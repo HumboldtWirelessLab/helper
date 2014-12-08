42540f75249354644924a2e2c79f86c70eb41176
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2014-12-08 16:58:59 +0100

    Channel models: remove old channel models, add ricean and nakagami

--- simulation/etc/ns/radio/802_11a.tcl
+++ simulation/etc/ns/radio/802_11a.tcl
@@ -32,6 +32,20 @@ Phy/WirelessPhy set Rate4 18e6
 Phy/WirelessPhy set Rate5 12e6
 Phy/WirelessPhy set Rate6 9e6
 Phy/WirelessPhy set Rate7 6e6
+
+if { $single_sensitivity == 1 } {
+
+Phy/WirelessPhy set RXThresh0 6.3096e-10; #-92db
+Phy/WirelessPhy set RXThresh1 6.3096e-10; #-92db
+Phy/WirelessPhy set RXThresh2 6.3096e-10; #-92db
+Phy/WirelessPhy set RXThresh3 6.3096e-10; #-92db
+Phy/WirelessPhy set RXThresh4 6.3096e-10; #-92db
+Phy/WirelessPhy set RXThresh5 6.3096e-10; #-92db
+Phy/WirelessPhy set RXThresh6 6.3096e-10; #-92db
+Phy/WirelessPhy set RXThresh7 6.3096e-10; #-92db
+
+} else {
+
 Phy/WirelessPhy set RXThresh0 3.28984e-09; #-84.8db
 Phy/WirelessPhy set RXThresh1 1.3096e-09; # ??db
 Phy/WirelessPhy set RXThresh2 9.3096e-10; # ??db
@@ -41,7 +55,9 @@ Phy/WirelessPhy set RXThresh5 6.3096e-10; #-92db
 Phy/WirelessPhy set RXThresh6 6.3096e-10; #-92db
 Phy/WirelessPhy set RXThresh7 6.3096e-10; #-92db
 
-#noise                                    
+}
+
+#noise
 
 # Mac Layer
 Mac/802_11 set dataRate_ 6Mb
