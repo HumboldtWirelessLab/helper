596f13db805e25eb98c81ceb5acec9b0d3fd4714
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2014-12-11 16:45:32 +0100

    Add new ChannelModel, add all channelmodels to tcl file. includes for channel model are no longer needed

--- simulation/etc/ns/script_02.tcl
+++ simulation/etc/ns/script_02.tcl
@@ -10,6 +10,16 @@ $ns_ at  $stoptime	"$ns_ nam-end-wireless $stoptime"
 
 
 puts "Starting Simulation..."
+puts "#Nodes: $nodecount"
+puts "Duration: $stoptime s"
+puts "Pathloss: $channel_model"
+puts "Shadowing-Pathloss: $shadowing_channel_model"
+puts "Fading: $fading_model"
+puts "Scheduler: $ns_scheduler"
+puts ""
+puts "Starting Simulation..."
+puts ""
+
 $ns_ run
 
 
