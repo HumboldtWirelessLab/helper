282ee9eecefb457d92e6c98723d75d76dac48abc
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2017-08-15 00:13:11 +0200

    Remove old eval stuff

--- evaluation/scenarios/old/outdoor/crcerror_plot_func.m
+++
@@ -1,3 +0,0 @@
-function crcerror_plot_func()
-crcerror_plot(SIZE,BITRATE,INTERVAL,'PACKETFILE','BITFILE');
-end
--- evaluation/scenarios/old/rssi_per-003/rssi_per_all.m
+++
@@ -1,6 +0,0 @@
-% render all plots
-for i=1:15
-    rssi_per(strcat( num2str(i), '_sender.dat'), strcat( num2str(i), '_receiver.dat'));
-end
-
-rssi_per_together();
