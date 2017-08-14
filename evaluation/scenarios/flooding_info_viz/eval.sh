23462de9f46f04736cc2a074526e8e2b130dacbb
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2017-08-15 00:12:00 +0200

    eval_viz: octave support

--- evaluation/scenarios/flooding_info_viz/eval.sh
+++ evaluation/scenarios/flooding_info_viz/eval.sh
@@ -30,22 +30,37 @@ fi
 
 EVALUATIONSDIR="$EVALUATIONSDIR""/flooding_info"
 
-echo "digraph G {" > $EVALUATIONSDIR/fwd_links.dot.tmp
+echo "digraph G {" > $EVALUATIONSDIR/fwd_links.dot
 
 if [ -e $NODEPLACEMENTFILE ]; then
   USE_NEATO=1
-  awk '{printf("\"%s\" [pos=\"%0.f,%0.f!\"];\n",$1,$2/100,$3/100) }' $NODEPLACEMENTFILE >> $EVALUATIONSDIR/fwd_links.dot.tmp
+  awk '{printf("\"%s\" [pos=\"%0.f,%0.f!\"];\n",$1,$2/100,$3/100) }' $NODEPLACEMENTFILE >> $EVALUATIONSDIR/fwd_links.dot
 else
   USE_NEATO=0
 fi
 
-RESULTDIR="../../"
+cat $EVALUATIONSDIR/flooding_lasthop_fwd_pkt_links.mat | awk '{ if ($3 > 0) print "\"<> " $1 " <>\" -> \"<> " $2 " <>\" [label=\""$3"\"];"}' | NUM2NAME=1 human_readable.sh $RESULTDIR/nodes.mac | sed -e "s#<> ##g" -e "s# <>##g" >> $EVALUATIONSDIR/fwd_links.dot
 
-cat $EVALUATIONSDIR/flooding_lasthop_fwd_pkt_links.mat | awk '{ if ($3 > 0) print "\"<> " $1 " <>\" -> \"<> " $2 " <>\" [label=\""$3"\"];"}' | NUM2NAME=1 human_readable.sh $RESULTDIR/nodes.mac | sed -e "s#<> ##g" -e "s# <>##g" >> $EVALUATIONSDIR/fwd_links.dot.tmp
+echo "}" >> $EVALUATIONSDIR/fwd_links.dot
 
-echo "}" >> $EVALUATIONSDIR/fwd_links.dot.tmp
 
-cat $EVALUATIONSDIR/fwd_links.dot.tmp > $EVALUATIONSDIR/fwd_links.dot
+DOTFILE=resp_graph.dot
+
+echo "digraph G {" > $EVALUATIONSDIR/$DOTFILE
+
+if [ -e $NODEPLACEMENTFILE ]; then
+  USE_NEATO=1
+  awk '{printf("\"%s\" [pos=\"%0.f,%0.f!\"];\n",$1,$2/100,$3/100) }' $NODEPLACEMENTFILE >> $EVALUATIONSDIR/$DOTFILE
+else
+  USE_NEATO=0
+fi
+
+
+cat $EVALUATIONSDIR/floodingforwardstats.mat | awk '{ if ($15 == 1) print $2" "$1;}' | sort -n | uniq -c | awk '{print "\"<> " $2 " <>\" -> \"<> " $3 " <>\" [label=\""$1"\"];"}' | NUM2NAME=1 human_readable.sh $RESULTDIR/nodes.mac | sed -e "s#<> ##g" -e "s# <>##g" >> $EVALUATIONSDIR/$DOTFILE
+
+echo "}" >> $EVALUATIONSDIR/$DOTFILE
+
+
 
 if [ ! -f $EVALUATIONSDIR/fwd_links.png ] || [ ! -f $EVALUATIONSDIR/fwd_links.eps ]; then
 
@@ -54,6 +69,7 @@ if [ ! -f $EVALUATIONSDIR/fwd_links.png ] || [ ! -f $EVALUATIONSDIR/fwd_links.ep
     dot -Teps $EVALUATIONSDIR/fwd_links.dot > $EVALUATIONSDIR/fwd_links.eps
   else
     neato -Teps $EVALUATIONSDIR/fwd_links.dot > $EVALUATIONSDIR/fwd_links.eps 2> /dev/null
+    neato -Teps $EVALUATIONSDIR/resp_graph.dot > $EVALUATIONSDIR/fwd_resp.eps 2> /dev/null
     if [ $? -ne 0 ]; then
       rm -f $EVALUATIONSDIR/fwd_links.eps
       neato -Tpng $EVALUATIONSDIR/fwd_links.dot > $EVALUATIONSDIR/fwd_links.png 2> /dev/null
@@ -66,4 +82,5 @@ if [ ! -f $EVALUATIONSDIR/fwd_links.png ] || [ ! -f $EVALUATIONSDIR/fwd_links.ep
   fi
 fi
 
-rm -f $EVALUATIONSDIR/fwd_links.dot.tmp $EVALUATIONSDIR/fwd_links.dot
+
+rm -f $EVALUATIONSDIR/fwd_links.dot $EVALUATIONSDIR/resp_graph.dot
--- evaluation/scenarios/network_info_viz/nodedegree_plot.m
+++ evaluation/scenarios/network_info_viz/nodedegree_plot.m
@@ -13,15 +13,15 @@ for i=1:size(psr,2)
   hist(h,max(h)-min(h)+1)
   print(strcat(finalfileprefix, 'nodedegree_hist_psr_', num2str(psr(i)), '.png'),'-dpng');
   print(strcat(finalfileprefix, 'nodedegree_hist_psr_', num2str(psr(i)), '.eps'),'-deps');
-  
+
   figure;
   cdfplot(h)
   print(strcat(finalfileprefix, 'nodedegree_cdf_psr_', num2str(psr(i)), '.png'),'-dpng');
   print(strcat(finalfileprefix, 'nodedegree_cdf_psr_', num2str(psr(i)), '.eps'),'-deps');
-  
+
   %[N,BIN] = histc(h,[min(h):max(h)]);
   %histogram = [[min(h):max(h)]; N]
- 
+
   %figure;
   %bar(histogram(1,:),histogram(2,:));
 
